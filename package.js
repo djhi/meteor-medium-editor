965d90c6f2f4cf99e74984589629b9c4493e7b89
CitizenKevin <Kevin@3GreatActs.com>
2014-10-15 18:39:07 -0500

    updated package.js with depedencies, version, and ignore to avoid original package folders

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-	name: "donflopez:medium-editor",
+	name: "citizenkevin:medium-editor",
 	summary: "Add medium-editor and medium insert clone to Meteor client side",
-	version: "1.2.0"
+	version: "1.2.1"
 });
 
 Package.on_use(function (api) {
+	api.versionsFrom('METEOR@0.9.2');
+  	api.use('jquery','client');
 	api.add_files(['medium-editor.css','medium-editor.js', 'medium-editor-insert-plugin.all.min.js','medium-editor-insert-plugin.css'], 'client');
 
 	if(api.export)
