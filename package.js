24361c9d6824b9be8cb649ff8a6b2a4ebca06040
CitizenKevin <Kevin@3GreatActs.com>
2014-10-15 20:14:37 -0500

    updated css

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
 	name: "citizenkevin:medium-editor",
-	summary: "Add medium-editor and medium insert clone to Meteor client side",
+	summary: "Add medium-editor clone and insert plugin to Meteor client side",
 	version: "1.2.3"
 });
 
 Package.on_use(function (api) {
 	api.versionsFrom('METEOR@0.9.0');
 	var files = [
-		'medium-editor.css',
 		'medium-editor.js', 
+		'medium-editor.css',
+		'default.css',
 		'insert-plugin/js/medium-editor-insert-plugin.all.js',
 		'insert-plugin/css/medium-editor-insert-plugin.css'
 	];
