08e5960fa0ab66cafd50edcfada12622581d3a9f
CitizenKevin <Kevin@3GreatActs.com>
2014-10-15 19:18:22 -0500

    minor adjustments made to medium js for variable export and bug fix

--- package.js
+++ package.js
@@ -1,13 +1,12 @@
 Package.describe({
 	name: "citizenkevin:medium-editor",
 	summary: "Add medium-editor and medium insert clone to Meteor client side",
-	version: "1.2.2"
+	version: "1.2.3"
 });
 
 Package.on_use(function (api) {
 	api.versionsFrom('METEOR@0.9.2');
-  	api.use('jquery','client');
-	api.add_files(['medium-editor.css','medium-editor.js'], 'client');
+	api.add_files(['medium-editor.css','medium-editor.js', 'medium-editor-insert-plugin.all.min.js','medium-editor-insert-plugin.css'], 'client');
 
 	if(api.export)
 		api.export('MediumEditor', 'client');
